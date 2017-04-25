e4add0c55b2d581901aae735769c726580473204
Nicole <nicolekentb@yahoo.com>
2017-04-25 12:36:26 -0600

    Redoing school project for practice. Involves basic HTML and CSS

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+$('.hamburger').click(function(e)
+{
+
+	$('nav').toggle();
+	e.preventDefault();
+})
+
+// $(document).ready(function()
+// {
+// 	$(window).on('resize', function()
+// 	{
+
+// 	});
+// });
